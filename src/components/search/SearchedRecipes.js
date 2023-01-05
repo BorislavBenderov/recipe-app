645eb92846e12c5d6c5e98d322e6e35318f0caf8
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-05 12:50:15 +0200

    Add Searched Recipes component

---
+++ src/components/search/SearchedRecipes.js
@@ -0,0 +1,10 @@
+export const SearchedRecipes = () => {
+    return (
+        <div className="max-w-5xl mx-auto mt-24">
+            <h1 className="text-xl font-bold text-center">Popular Recipes</h1>
+            <div className="flex flex-wrap justify-between">
+                
+            </div>
+        </div>
+    );
+}

6c3c377d385a8792020f38565734035ce5a1f5f2
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-05 13:58:41 +0200

    Add SearchedRecipeCard

---
+++ src/components/search/SearchedRecipeCard.js
@@ -0,0 +1,8 @@
+export const SearchedRecipeCard = ({ recipe }) => {
+    return (
+        <div className="w-80 my-10">
+            <img src={recipe.image} alt="recipe" className="rounded-xl" />
+            <p className="text-center">{recipe.title}</p>
+        </div>
+    );
+}

a4a7333699873a26471d2c024782cc1b8e206e21
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-05 12:47:32 +0200

    Fetch searched recipes

--- src/api/index.js
+++ src/api/index.js
@@ -4,3 +4,10 @@ export const fetchRandomRecipes = () => {
             return res.json();
         })
 }
+
+export const fetchSearch = (query) => {
+    return fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${query}&number=9`)
+        .then((res) => {
+            return res.json();
+        })
+}

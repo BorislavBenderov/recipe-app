cfac57cd00a020ddae1d069c7627289b79eec5b2
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-05 13:04:11 +0200

    Wrap app with RecipeContextProvider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import { PopularRecipes } from "./components/popular-recipes/PopularRecipes";
 import { Search } from "./components/search/Search";
+import { SearchedRecipes } from "./components/search/SearchedRecipes";
+import { RecipeContextProvider } from "./contexts/RecipeContext";
 
 function App() {
 
 
   return (
-    <div className="App">
-      <Search />
-      <PopularRecipes />
-    </div>
+    <RecipeContextProvider>
+      <div className="App">
+        <Search />
+        <Routes>
+          <Route path="/" element={<PopularRecipes />} />
+          <Route path="/search/:query" element={<SearchedRecipes />} />
+        </Routes>
+      </div>
+    </RecipeContextProvider>
   );
 }
 

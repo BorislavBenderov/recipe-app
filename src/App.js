23b24d8ade71e041626b6b4d08f9fad7e98c72ad
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-05 12:47:39 +0200

    Render Search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { PopularRecipes } from "./components/popular-recipes/PopularRecipes";
+import { Search } from "./components/search/Search";
 
 function App() {
 
 
   return (
     <div className="App">
+      <Search />
       <PopularRecipes />
     </div>
   );

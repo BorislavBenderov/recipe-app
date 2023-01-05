a28178876a5484d06e5decd17a171c8a6f8bfc6d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-05 13:59:39 +0200

    Add onSearch function to take query

--- src/contexts/RecipeContext.js
+++ src/contexts/RecipeContext.js
@@ -5,6 +5,7 @@ export const RecipeContext = createContext();
 
 export const RecipeContextProvider = ({ children }) => {
     const [randomRecipes, setRandomRecipes] = useState([]);
+    const [query, setQuery] = useState('');
 
     useEffect(() => {
         fetchRandomRecipes()
@@ -12,8 +13,13 @@ export const RecipeContextProvider = ({ children }) => {
                 setRandomRecipes(res.recipes);
             })
     }, []);
+
+    const onSearch = (searchWord) => {
+        setQuery(searchWord);
+    }
+
     return (
-        <RecipeContext.Provider value={{ randomRecipes }}>
+        <RecipeContext.Provider value={{ randomRecipes, query, onSearch }}>
             {children}
         </RecipeContext.Provider>
     );

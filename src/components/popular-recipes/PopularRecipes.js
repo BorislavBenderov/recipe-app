ebd706ba369ae2d6dfa95f4852bbb67899e1f9b9
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-03 20:11:19 +0200

    Use isError and error

--- src/components/popular-recipes/PopularRecipes.js
+++ src/components/popular-recipes/PopularRecipes.js
@@ -3,20 +3,25 @@ import { PopularRecipeCard } from "./PopularRecipeCard";
 import { RecipeContext } from "../../contexts/RecipeContext";
 
 export const PopularRecipes = () => {
-  const { randomRecipes, isLoading } = useContext(RecipeContext);
+  const { randomRecipes, isLoading, isError, error } =
+    useContext(RecipeContext);
+
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (isError) {
+    content = <p>{error.message}</p>;
+  } else {
+    content = randomRecipes?.map((recipe) => (
+      <PopularRecipeCard key={recipe.id} recipe={recipe} />
+    ));
+  }
 
   return (
     <div className="max-w-5xl mx-auto mt-24">
       <h1 className="text-xl font-bold text-center">Popular Recipes</h1>
-      <div className="flex flex-wrap justify-between">
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          randomRecipes?.map((recipe) => (
-            <PopularRecipeCard key={recipe.id} recipe={recipe} />
-          ))
-        )}
-      </div>
+      <div className="flex flex-wrap justify-between">{content}</div>
     </div>
   );
 };

a1f807f22b80e4561ae91b8cf8ad1f70f8381f85
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-03 18:56:20 +0200

    Use axios for fetchRandomRecipes

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,16 @@
-export const fetchRandomRecipes = () => {
-    return fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
-        .then((res) => {
-            return res.json();
-        })
-}
+import axios from "axios";
+
+export const fetchRandomRecipes = async () => {
+  const response = await axios.get(
+    `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+  );
+  return response.data.recipes;
+};
 
 export const fetchSearch = (query) => {
-    return fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${query}&number=9`)
-        .then((res) => {
-            return res.json();
-        })
-}
+  return fetch(
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${query}&number=9`
+  ).then((res) => {
+    return res.json();
+  });
+};

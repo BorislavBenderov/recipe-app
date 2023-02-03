d09de4b2883c827127416b1e38f9ff8bb513643d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-03 19:19:30 +0200

    Use isLoading

--- src/components/popular-recipes/PopularRecipes.js
+++ src/components/popular-recipes/PopularRecipes.js
@@ -3,14 +3,20 @@ import { PopularRecipeCard } from "./PopularRecipeCard";
 import { RecipeContext } from "../../contexts/RecipeContext";
 
 export const PopularRecipes = () => {
-    const { randomRecipes } = useContext(RecipeContext);
+  const { randomRecipes, isLoading } = useContext(RecipeContext);
 
-    return (
-        <div className="max-w-5xl mx-auto mt-24">
-            <h1 className="text-xl font-bold text-center">Popular Recipes</h1>
-            <div className="flex flex-wrap justify-between">
-                {randomRecipes?.map((recipe) => <PopularRecipeCard key={recipe.id} recipe={recipe} />)}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="max-w-5xl mx-auto mt-24">
+      <h1 className="text-xl font-bold text-center">Popular Recipes</h1>
+      <div className="flex flex-wrap justify-between">
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          randomRecipes?.map((recipe) => (
+            <PopularRecipeCard key={recipe.id} recipe={recipe} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};

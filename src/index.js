c5e6fec275329f9c0ace777cb891e72d55266cc4
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-04 08:34:13 +0200

    Refetch data only once

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnmount: false,
+      refetchOnReconnect: false,
+      retry: false,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

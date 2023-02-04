2760a6a2b6f3696e62843e44e527b0182eceb93d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-04 08:21:11 +0200

    Use react query to fetch searched data

--- src/components/search/SearchedRecipes.js
+++ src/components/search/SearchedRecipes.js
@@ -1,28 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
 import { useContext, useEffect, useState } from "react";
 import { fetchSearch } from "../../api";
 import { RecipeContext } from "../../contexts/RecipeContext";
 import { SearchedRecipeCard } from "./SearchedRecipeCard";
 
 export const SearchedRecipes = () => {
-    const { query } = useContext(RecipeContext);
-    const [searchRecipes, setSearchRecipes] = useState([]);
+  const { query } = useContext(RecipeContext);
 
-    useEffect(() => {
-        fetchSearch(query)
-            .then((res) => {
-                setSearchRecipes(res.results);
-            })
-    }, [query]);
+  const { data: searchRecipes } = useQuery({
+    queryKey: ["searchRecipes"],
+    queryFn: () => fetchSearch(query),
+  });
 
-    return (
-        <div className="max-w-5xl mx-auto mt-24">
-            <h1 className="text-xl font-bold text-center">Popular Recipes with {query[0]?.toUpperCase() + query?.slice(1)?.toLowerCase()}</h1>
-            {searchRecipes.length > 0
-                ? <div className="flex flex-wrap justify-between">
-                    {searchRecipes.map((recipe) => <SearchedRecipeCard key={recipe.id} recipe={recipe} />)}
-                </div>
-                : <p className="text-center ml-10 text-xl mt-10">No recipes found!</p>}
-
+  return (
+    <div className="max-w-5xl mx-auto mt-24">
+      <h1 className="text-xl font-bold text-center">
+        Popular Recipes with{" "}
+        {query[0]?.toUpperCase() + query?.slice(1)?.toLowerCase()}
+      </h1>
+      {searchRecipes?.length > 0 ? (
+        <div className="flex flex-wrap justify-between">
+          {searchRecipes?.map((recipe) => (
+            <SearchedRecipeCard key={recipe.id} recipe={recipe} />
+          ))}
         </div>
-    );
-}
+      ) : (
+        <p className="text-center ml-10 text-xl mt-10">No recipes found!</p>
+      )}
+    </div>
+  );
+};

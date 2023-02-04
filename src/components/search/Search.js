92168a500583af0118cfa1758b60bbc9a7e1e14c
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-04 08:30:49 +0200

    Add onClose action

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,29 +3,44 @@ import { useNavigate } from "react-router-dom";
 import { RecipeContext } from "../../contexts/RecipeContext";
 
 export const Search = () => {
-    const { onSearch } = useContext(RecipeContext);
-    const [searchWord, setSearchWord] = useState('');
-    const navigate = useNavigate();
+  const { onSearch } = useContext(RecipeContext);
+  const [searchWord, setSearchWord] = useState("");
+  const navigate = useNavigate();
 
-    const onSearchClick = (e) => {
-        e.preventDefault();
+  const onSearchClick = (e) => {
+    e.preventDefault();
 
-        onSearch(searchWord);
-        navigate(`/search/${searchWord}`);
-    }
+    onSearch(searchWord);
+    navigate(`/search/${searchWord}`);
+  };
 
-    return (
-        <div className="w-60 mx-auto mt-6">
-            <form onSubmit={onSearchClick}>
-                <label htmlFor="search" />
-                <input
-                    className='w-60 bg-black text-white'
-                    type="text"
-                    placeholder="Search recipes" id="search"
-                    name="search"
-                    value={searchWord}
-                    onChange={(e) => setSearchWord(e.target.value)} />
-            </form>
-        </div>
-    );
-}
+  const onClose = () => {
+    navigate("/");
+    setSearchWord("");
+  };
+
+  return (
+    <div className="w-60 mx-auto mt-6 relative">
+      <form onSubmit={onSearchClick}>
+        <label htmlFor="search" />
+        <input
+          className="w-60 bg-black text-white"
+          type="text"
+          placeholder="Search recipes"
+          id="search"
+          name="search"
+          value={searchWord}
+          onChange={(e) => setSearchWord(e.target.value)}
+        />
+      </form>
+      {searchWord ? (
+        <button
+          onClick={onClose}
+          className="absolute top-[-2px] left-[230px] text-white"
+        >
+          x
+        </button>
+      ) : null}
+    </div>
+  );
+};
